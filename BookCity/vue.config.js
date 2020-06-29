bc4cefeeb13402c0dcadeb384b8ad94c9b5d14ea
hong yang <you@example.com>
2020-06-29 19:24:09 +0800

    打包完成

--- BookCity/vue.config.js
+++ BookCity/vue.config.js
@@ -2,6 +2,11 @@ const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
+
+  css: {
+    extract: false
+  },
+
   chainWebpack: config => {
     // 设置快捷路径， @ 表示 'src' ，components 表示 'src/components'
     config.resolve.alias
